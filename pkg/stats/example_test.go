1436aa2a56d370d0bdc3e2e1c689c52c15d6e07a
Dilshod K <d.kobilov@mail.ru>
2021-11-15 00:24:38 +0500

    added Status to all

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -3,39 +3,45 @@ package stats
 import (
 	"fmt"
 
-	"github.com/DilshodK0592/bank/pkg/types"
+	"github.com/DilshodK0592/bank/v2/pkg/types"
 )
 
 func ExampleAvg() {
 	pay := []types.Payment {
 		{
 			Amount: 1_000_00,
+			Status: "OK",
 		},
 		{
 			Amount: 2_000_00,
+			Status: "FAIL",
 		},
 		{
-			Amount: 3_000_00,
+			Amount: 5_000_00,
+			Status: "INPROGRESS",
 		},
 	}
 	result := Avg(pay)
 	fmt.Println(result)
-	//Output: 200000
+	//Output: 300000
 }
 
 func ExampleTotalInCategory() {
 	pays := []types.Payment {
 		{
 			Category: "auto",
-			Amount: 1_000_00,
+			Amount: 2_000_00,
+			Status: "OK",
+		},
+		{
+			Category: "auto",
+			Amount: 3_000_00,
+			Status: "INPROGRESS",
 		},
 		{
 			Category: "auto",
 			Amount: 1_000_00,
-		},
-		{
-			Category: "food",
-			Amount: 1_000_00,
+			Status: "FAIL",
 		},
 	}
 
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,24 +1,32 @@
 package stats
 
-import "github.com/DilshodK0592/bank/pkg/types"
+import "github.com/DilshodK0592/bank/v2/pkg/types"
 
 // Avg рассчитывает среднюю сумму платежа.
 func Avg(payments []types.Payment) types.Money {
 	pay:= types.Money(0)
+	indexPay := 0
 	for _, payment := range payments {
+		if payment.Status != types.StatusFail {
 		pay += payment.Amount
+		indexPay++
+		}
 	}
-	averagePay := pay/types.Money(len(payments))
+	averagePay := pay/types.Money(indexPay)
 	return averagePay
 }
 
 // TotalInCategory находит сумму покупок в опредённой категории.
-func TotalInCategory(payments []types.Payment, categoty types.Category) types.Money {
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	pay:= types.Money(0)
 	for _, payment := range payments {
-		if categoty == payment.Category {
-		pay += payment.Amount
+		if payment.Status == types.StatusFail {
+			continue
 		}
+		if category != payment.Category {
+			continue
+		}
+		pay += payment.Amount
 	}
 	
 	return pay
